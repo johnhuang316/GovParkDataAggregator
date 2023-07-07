e80cc643bbcefac8ff31810b737df076caf6f1be
John <john700@pic.net.tw>
2023-07-07 15:05:43 +0800

    bug fix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from datastorage.bigquerystorage import BigQueryStorage
 from repository.parkinglotrepository import ParkingLotPepository
+from repository.parkingavailabilityrepository import ParkingAvailabilityRepository
 from api.apifactory import get_api
 from process.updateprakinglotprocess import UpdateParkingLotProcess
 from process.logparkingavailability import LogParkingAvailability
@@ -17,16 +18,20 @@ def main():
     action = os.getenv("ACTION", default="")
     match  action:
         case "update_parking_lot":
+            repository = ParkingLotPepository(BigQueryStorage())
             process = UpdateParkingLotProcess(repository, api_list)
         case "log_parking_availability":
+            repository = ParkingAvailabilityRepository(BigQueryStorage())
             process = LogParkingAvailability(repository, api_list)
         case "single_log_parking_availability":
             api_name = os.getenv("API", default="")
             single_api_list = [
                 get_api(api_name)
             ]
+            repository = ParkingAvailabilityRepository(BigQueryStorage())
             process = LogParkingAvailability(repository, single_api_list)
         case "reset_parking_availability":
+            repository = ParkingAvailabilityRepository(BigQueryStorage())
             process = ResetParkingAvailability(repository)
         case _:
             raise ValueError("no action")
